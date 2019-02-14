bf2e0a3d24073f2156332d5adfd9384bb92c97bb
Isaac Kwon <isaac-kwon@hotmail.com>
2019-02-14 14:33:43 -0800

    lazyload images

--- client/src/components/PokemonData/PokemonCard.js
+++ client/src/components/PokemonData/PokemonCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, Col } from 'antd';
 import { connect } from 'react-redux';
+import LazyLoad from 'react-lazyload';
 import { fetchSelectedPokemonData } from '../../actions';
 const { Meta } = Card;
 
@@ -19,22 +20,27 @@ class PokemonCard extends Component {
 
     return (
       <Col xs={24} sm={12} md={8} lg={6} xl={3}>
-        <Card
-          className='pokemon-card'
-          style={style.card}
-          onClick={() => fetchSelectedPokemonData(data.name)}
-          cover={
-            <img
-              alt='pokemon'
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
-              style={style.image}
+          <Card
+            className='pokemon-card'
+            style={style.card}
+            onClick={() => fetchSelectedPokemonData(data.name)}
+            cover={
+              <LazyLoad
+                height={'100%'}
+                once
+              >
+                <img
+                  alt='pokemon'
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
+                  style={style.image}
+                />
+              </LazyLoad>
+            }
+            >
+            <Meta
+              title={data.name}
             />
-          }
-        >
-          <Meta
-            title={data.name}
-          />
-        </Card>
+          </Card>
       </Col>
     );
   }
