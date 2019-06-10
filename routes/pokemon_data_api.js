2b6c4b107a618a4773f129b3e5ebcf1483bd14c5
isaac1104 <isaac-kwon@hotmail.com>
2019-06-10 15:24:27 +0900

    send status 200 if fetching is successful

--- routes/pokemon_data_api.js
+++ routes/pokemon_data_api.js
@@ -4,12 +4,12 @@ module.exports = app => {
   app.get('/api/pokemon', async (req, res) => {
     const request = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151');
     const { data } = request;
-    res.send(data);
+    res.status(200).send(data);
   });
 
   app.get('/api/selectedPokemon', async (req, res) => {
     const request = await axios.get(`https://pokeapi.co/api/v2/pokemon/${req.query.pokemon}`);
     const { data } = request;
-    res.send(data);
+    res.status(200).send(data);
   });
 }
